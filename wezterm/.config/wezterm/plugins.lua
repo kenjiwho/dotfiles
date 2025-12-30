0600526c227cbf08a9be40863f858df3aea79232
kenjiwho <kenji.opdam@gmail.com>
2025-12-30 20:23:03 +0100

    remove unnecessary configurations

--- wezterm/.config/wezterm/plugins.lua
+++
@@ -1,5 +0,0 @@
-local wezterm = require("wezterm")
-
-SmartSplits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
-WorkspaceSwitcher = wezterm.plugin.require("https://github.com/konjiii/workspace_switcher.wezterm")
-Sessionizer = wezterm.plugin.require("https://github.com/mikkasendke/sessionizer.wezterm")
