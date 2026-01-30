0c91df7d69fddb2d0238b4d874040369fba782b8
konjiii <kenji.opdam@gmail.com>
2026-01-30 02:37:53 +0100

    remove old treesitter config command

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,9 +2,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
-    end,
     opts = {
         ensure_installed = {
             "markdown",
