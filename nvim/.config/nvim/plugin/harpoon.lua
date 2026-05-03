1cbdc528021f0c230b4b46900c07b0dc1134abf9
konjiii <kenji.opdam@gmail.com>
2026-05-03 16:25:37 +0200

    add primeagen's 99 ai agent

--- nvim/.config/nvim/plugin/harpoon.lua
+++ nvim/.config/nvim/plugin/harpoon.lua
@@ -18,5 +18,4 @@ vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
 vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
 vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
 vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
-vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end)
 -- stylua: ignore end
